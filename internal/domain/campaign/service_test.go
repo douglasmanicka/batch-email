f82bb1e0f5d6d99147bf9b12cb7623624c269f9c
douglasmanicka <danidev2020@gmail.com>
2023-07-17 18:45:34 -0300

    service tests

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/douglasmanicka/batch-email/internal/contract"
@@ -17,27 +18,37 @@ func (r *repositoryMock) Save(campaign *Campaign) error {
 	return args.Error(0)
 }
 
-func Test_Create_Campaign(t *testing.T) {
-	assert := assert.New(t)
-	service := Service{}
-	newCampaign := contract.NewCampaign{
+var (
+	newCampaign = contract.NewCampaign{
 		Name:    "Test y",
 		Content: "Body",
 		Emails:  []string{"test@test.com"},
 	}
+	newCampaign2 = contract.NewCampaign{
+		Name:    "Test z",
+		Content: "Body2",
+		Emails:  []string{"test2@test.com"},
+	}
+
+	service = Service{}
+)
+
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
+
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("Save", mock.Anything).Return(nil)
+
+	service.Repository = repositoryMock
 
 	id, err := service.Create(newCampaign)
 	assert.NotEmpty(id)
+	//assert.NotNil(id)
 	assert.Nil(err)
 
 }
 
 func Test_Create_SaveCampaign(t *testing.T) {
-	newCampaign := contract.NewCampaign{
-		Name:    "Test y",
-		Content: "Body",
-		Emails:  []string{"test@test.com"},
-	}
 	repositoryMock := new(repositoryMock)
 	//for quick test use below
 	// repositoryMock.On("Save", mock.Anything).Return(nil)
@@ -50,7 +61,30 @@ func Test_Create_SaveCampaign(t *testing.T) {
 		return true
 	})).Return(nil)
 
-	service := Service{Repository: repositoryMock}
+	//service := Service{Repository: repositoryMock}
+	service.Repository = repositoryMock
 	service.Create(newCampaign)
 	repositoryMock.AssertExpectations(t)
+}
+
+func Test_Create_Campaign_ValidateDomainError(t *testing.T) {
+	assert := assert.New(t)
+	newCampaign.Name = ""
+	_, err := service.Create(newCampaign)
+	assert.NotNil(err)
+	assert.Equal("name is required", err.Error())
+
+}
+
+func Test_Create_ValidateRepositorySaveCampaign(t *testing.T) {
+	assert := assert.New(t)
+	repositoryMock := new(repositoryMock)
+	//for quick test use below
+	repositoryMock.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+
+	service.Repository = repositoryMock
+
+	_, err := service.Create(newCampaign2)
+	assert.Equal("error to save on database", err.Error())
+
 }
